279fdb2443a69371d8153e492dca9aff440ce30f
zerosalt1412 <sikarin2006f@gmail.com>
2025-11-18 13:58:29 +0700

    Submit exercise Labsheet101

---
+++ Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("I am Malee AROINA");
+        //System.out.println("\"INT_108\" Object-Oriented Programming");
+        System.out.println("\tThis is my first time learning Java");
+        System.out.println("\tI hope to get grade \"A\"");
+    }
+}
---
+++ Lab105.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Summation number = " + (2+5) +
+                        "\nSubtraction number = " + (2 - 5) +
+                        "\nMultiplication number = " + (2*5) +
+                        "\nDivision number = " + (2.0 / 5.0) +
+                        "\nModulus number = " + (2 % 5),
+                "Arithmetic Operators",
+                JOptionPane.WARNING_MESSAGE
+
+
+
+                );
+    }
+}
